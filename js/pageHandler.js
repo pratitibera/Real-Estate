bdb14ad1077087620ddf7d9d7c0da100381811e6
Pratiti Bera <remabimal0801@gmail.com>
2021-03-07 00:08:07 +0530

    complete

--- js/pageHandler.js
+++ js/pageHandler.js
@@ -6,3 +6,13 @@ function raiseTicket(){
 	document.getElementById('view-status-section').style.display = "none";
 	document.getElementById('raise-ticket-section').style.display = "block";
 }
+
+function option(){
+	document.getElementById('raise-ticket-section').style.display = "none";
+	document.getElementById('raise-issue-section').style.display = "block";
+}
+
+function issueDetails(){
+	document.getElementById('issueDetails').style.display = "flex";
+	document.getElementById('issueDetailsImage').style.display = "flex";
+}
