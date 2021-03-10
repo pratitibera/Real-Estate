0ec9e0234c55f7a81b8113bc8178d36c97f8481e
Pratiti Bera <remabimal0801@gmail.com>
2021-03-11 01:51:10 +0530

    Dashboard

--- js/pageHandler.js
+++ js/pageHandler.js
@@ -16,3 +16,25 @@ function issueDetails(){
 	document.getElementById('issueDetails').style.display = "flex";
 	document.getElementById('issueDetailsImage').style.display = "flex";
 }
+function sidebar(){
+	$(".humberger__menu__wrapper").addClass("show__humberger__menu__wrapper");
+         $(".humberger__menu__overlay").addClass("active");
+         $("body").addClass("over_hid");
+}
+
+(function ($) {
+         //Humberger Menu
+         $(".humberger__open").on("click", function () {
+         $(".humberger__menu__wrapper").addClass("show__humberger__menu__wrapper");
+         $(".humberger__menu__overlay").addClass("active");
+         $("body").addClass("over_hid");
+         });
+         
+         $(".humberger__menu__overlay").on("click", function () {
+         $(".humberger__menu__wrapper").removeClass(
+         "show__humberger__menu__wrapper"
+         );
+         $(".humberger__menu__overlay").removeClass("active");
+         $("body").removeClass("over_hid");
+         });
+         })(jQuery);
