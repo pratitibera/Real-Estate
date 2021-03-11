d70e52ec5e2bb2b3c9e1683dddbfc8c2ce1f25d9
Pratiti Bera <remabimal0801@gmail.com>
2021-03-11 23:31:22 +0530

    Dashboard

--- js/pageHandler.js
+++ js/pageHandler.js
@@ -1,30 +1,43 @@
-function viewStatus(){
-	document.getElementById('view-status-section').style.display = "block";
-	document.getElementById('raise-ticket-section').style.display = "none";
-}
-function raiseTicket(){
-	document.getElementById('view-status-section').style.display = "none";
-	document.getElementById('raise-ticket-section').style.display = "block";
+function viewStatus() {
+  document.getElementById('view-status-section').style.display = "block";
+  document.getElementById('raise-ticket-section').style.display = "none";
 }
 
-function option(){
-	document.getElementById('raise-ticket-section').style.display = "none";
-	document.getElementById('raise-issue-section').style.display = "block";
+function raiseTicket() {
+  document.getElementById('view-status-section').style.display = "none";
+  document.getElementById('raise-ticket-section').style.display = "block";
 }
 
-function issueDetails(){
-	document.getElementById('issueDetails').style.display = "flex";
-	document.getElementById('issueDetailsImage').style.display = "flex";
-}
-function sidebar(){
-	$(".humberger__menu__wrapper").addClass("show__humberger__menu__wrapper");
-         $(".humberger__menu__overlay").addClass("active");
-         // $("body").addClass("over_hid");
+function option() {
+  document.getElementById('raise-ticket-section').style.display = "none";
+  document.getElementById('raise-issue-section').style.display = "block";
 }
 
-function ticketDetails(){
-            document.getElementById('admin-ticketDetails').style.display = "block";
-         }
+function issueDetails() {
+  document.getElementById('issueDetails').style.display = "flex";
+  document.getElementById('issueDetailsImage').style.display = "flex";
+}
+
+function sidebar() {
+  $(".humberger__menu__wrapper").addClass("show__humberger__menu__wrapper");
+  $(".humberger__menu__overlay").addClass("active");
+}
+
+function ticketDetails() {
+  document.getElementById('admin-ticketDetails').style.display = "block";
+}
+
+function showsection2() {
+  document.getElementById('section2').style.display = "block";
+  document.getElementById('section1').style.display = "none";
+  document.getElementById('admin-ticketDetails').style.display = "none";
+}
+
+function showsection1() {
+  document.getElementById('section2').style.display = "none";
+  document.getElementById('section1').style.display = "block";
+  document.getElementById('admin-ticketDetails').style.display = "none";
+}
 
 (function ($) {
 
