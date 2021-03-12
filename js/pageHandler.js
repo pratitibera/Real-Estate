519a804b61ef49cf0724ec0964cbfb0d2a048da0
Pratiti Bera <remabimal0801@gmail.com>
2021-03-12 12:56:04 +0530

    Corrections

--- js/pageHandler.js
+++ js/pageHandler.js
@@ -31,12 +31,24 @@ function showsection2() {
   document.getElementById('section2').style.display = "block";
   document.getElementById('section1').style.display = "none";
   document.getElementById('admin-ticketDetails').style.display = "none";
+
+  $(".humberger__menu__wrapper").removeClass(
+      "show__humberger__menu__wrapper"
+    );
+    $(".humberger__menu__overlay").removeClass("active");
+    $("body").removeClass("over_hid");
 }
 
 function showsection1() {
   document.getElementById('section2').style.display = "none";
   document.getElementById('section1').style.display = "block";
   document.getElementById('admin-ticketDetails').style.display = "none";
+
+  $(".humberger__menu__wrapper").removeClass(
+      "show__humberger__menu__wrapper"
+    );
+    $(".humberger__menu__overlay").removeClass("active");
+    $("body").removeClass("over_hid");
 }
 
 (function ($) {
